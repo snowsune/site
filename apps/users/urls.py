3ead2b540418f10ba3516952c3b0947a0af021b3
Snowsune <anonymousfyf@gmail.com>
2025-04-11 16:48:08 -0400

    Oops! Forgor login :P

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,11 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="users/login.html"),
-        name="login",
-    ),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
     path("register/", views.register_view, name="register"),
     path("edit/", views.edit_account_view, name="account-edit"),
