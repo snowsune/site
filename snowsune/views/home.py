0045392206067c00a41481d31a39618f2c4364c7
Snowsune <anonymousfyf@gmail.com>
2025-08-09 23:34:19 -0400

    Add blogpost to front page

--- snowsune/views/home.py
+++ snowsune/views/home.py
@@ -1,7 +1,17 @@
 from django.views import View
 from django.shortcuts import render
+from apps.blog.models import BlogPost
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "home.html")
+        # Get the latest published blog post
+        latest_post = (
+            BlogPost.objects.filter(status="published")
+            .order_by("-published_at", "-created_at")
+            .first()
+        )
+
+        context = {"latest_post": latest_post}
+
+        return render(request, "home.html", context)
