aea59ef4aaea164ff08d41e5291f2e9847213feb
Snowsune <anonymousfyf@gmail.com>
2025-08-14 20:49:52 -0400

    Add in RSS feed

--- apps/comics/urls.py
+++ apps/comics/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from . import views
+from . import views, feeds
 
 app_name = "comics"
 
 urlpatterns = [
     path("", views.comic_home, name="comic_home"),
     path("page/<int:page_number>/", views.page_detail, name="page_detail"),
-    path("page/<int:page_number>/navigation/", views.page_navigation, name="page_navigation"),
+    path(
+        "page/<int:page_number>/navigation/",
+        views.page_navigation,
+        name="page_navigation",
+    ),
+    path("feed/", feeds.ComicsFeed(), name="comic_feed"),
 ]
