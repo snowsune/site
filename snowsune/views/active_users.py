d07fc3a4c753e76a2ae89e7c17278193e2e31a46
Snowsune <anonymousfyf@gmail.com>
2025-08-15 21:15:58 -0400

    Slightly more accurate page views tracking

--- snowsune/views/active_users.py
+++ snowsune/views/active_users.py
@@ -1,26 +1,14 @@
 from django.http import JsonResponse
 from django.utils import timezone
-from datetime import timedelta
-from tracking.models import Visitor
+from django.contrib.sessions.models import Session
 
 
 def active_users_view(request):
-    """Return the number of currently active users (sessions started in last 30 minutes)"""
+    """Return the number of currently active users based on active sessions"""
 
-    # Consider users active if they started a session in the last 30 minutes
-    # This is more realistic than counting non-expired sessions (which can last weeks)
+    # Count active (non-expired) sessions
+    # This shows users who are actually connected to the site
     now = timezone.now()
-    thirty_minutes_ago = now - timedelta(minutes=30)
-
-    # Count unique IPs that started sessions in the last 30 minutes
-    active_users = (
-        Visitor.objects.filter(
-            start_time__gte=thirty_minutes_ago,  # Session started recently
-            start_time__lte=now,  # Session started before now
-        )
-        .values("ip_address")
-        .distinct()
-        .count()
-    )
+    active_users = Session.objects.filter(expire_date__gt=now).count()
 
     return JsonResponse({"active_users": active_users, "timestamp": now.isoformat()})
