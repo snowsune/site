5a5189cf0f399945f2e28801bae67500e13bfcc3
Snowsune <vixi@snowsune.net>
2025-10-18 17:23:03 -0400

    Improve fops redir link

--- apps/bot_manager/views/redirect.py
+++ apps/bot_manager/views/redirect.py
@@ -6,4 +6,13 @@ def fops_redirect_view(request):
     Super simple redirect for fops. Wont show a preview.
     Google analytics *should* track the redirect.
     """
-    return render(request, "bot_manager/fops_redirect.html")
+
+    # For google analytics tracking
+    # https://support.google.com/analytics/thread/267260949/how-to-track-domain-before-it-redirects?hl=en
+    redirect_url = (
+        "/fops/?utm_source=redirect&utm_medium=meta_refresh&utm_campaign=fops_redirect"
+    )
+
+    context = {"redirect_url": redirect_url}
+
+    return render(request, "bot_manager/fops_redirect.html", context)
